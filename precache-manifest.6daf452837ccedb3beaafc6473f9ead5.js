40463644d53ff5ec2aeaa27865646f48bcb110ab
Gonçalo Ferreira <1170905@isep.ipp.pt>
2020-07-26 23:09:14 +0100

    Updates

---
+++ precache-manifest.6daf452837ccedb3beaafc6473f9ead5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd53cfa4f05a5e8dbe6374c9a46b5f8d",
+    "url": "/taodsite/index.html"
+  },
+  {
+    "revision": "ec3462752ccb44744c04",
+    "url": "/taodsite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cf43f3ce6548d282074b",
+    "url": "/taodsite/static/js/2.8b537c1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/taodsite/static/js/2.8b537c1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec3462752ccb44744c04",
+    "url": "/taodsite/static/js/main.a998bf4d.chunk.js"
+  },
+  {
+    "revision": "c98ffe7d5e88af4f084f",
+    "url": "/taodsite/static/js/runtime-main.c2faf24c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/taodsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
