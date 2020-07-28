8b8ae8deee3350229c12cad91da17e6c86232096
Gonçalo Ferreira <1170905@isep.ipp.pt>
2020-07-28 13:42:06 +0100

    Updates

---
+++ precache-manifest.563f9c5face91859c1435c1ddaaac9c0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd4b8dfd56609930e779fa76a1a4ac29",
+    "url": "/taodsite/index.html"
+  },
+  {
+    "revision": "627ba890353873b8163e",
+    "url": "/taodsite/static/css/main.5a19319f.chunk.css"
+  },
+  {
+    "revision": "c6c8e144460205a4d13d",
+    "url": "/taodsite/static/js/2.f037ee68.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/taodsite/static/js/2.f037ee68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "627ba890353873b8163e",
+    "url": "/taodsite/static/js/main.86cf2130.chunk.js"
+  },
+  {
+    "revision": "9b7f4289563fe7941f19",
+    "url": "/taodsite/static/js/runtime-main.c5e94ac5.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/taodsite/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/taodsite/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/taodsite/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/taodsite/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/taodsite/static/media/landing-bg.df8fd3ef.jpg"
+  }
+]);
