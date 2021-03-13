c14d903b35b132541989cedad8af3a4d75647fa4
Joao Nascimento <61145709+joao-vitor-nascimento@users.noreply.github.com>
2021-03-13 16:33:00 +0000

    Mudei os butões para ficarem estilosos

--- src/views/Socio/SocioPage.js
+++ src/views/Socio/SocioPage.js
@@ -2,7 +2,7 @@ import React from "react";
 // nodejs library that concatenates classes
 import classNames from "classnames";
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
 // @material-ui/icons
 
@@ -28,28 +28,42 @@ const dashboardRoutes = [];
 
 const useStyles = makeStyles(styles);
 
-  const carousel_settings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        focusOnSelect: true
-    };
+const DownloadButton = withStyles({
+  root: {
+    background: "#000000",
+    borderRadius: 3,
+    border: 0,
+    color: "white",
+    height: 35,
+    marginLeft: 10,
+    marginBottom: 2,
+    marginRight: 10,
+
+  },
+})(Button);
+
+const carousel_settings = {
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  focusOnSelect: true
+};
 
 
 function getImages() {
-        var result = patrocinios.items.map(function (i, index) {
-            var foto = require("assets/img/patrocinios/" + i.foto + ".jpg");
-            return (
-                <div>
-                    <img src={foto} alt={index} className="slick-image" />
-                </div>
-            )
-        })
-        return result;
-    }
+  var result = patrocinios.items.map(function (i, index) {
+    var foto = require("assets/img/patrocinios/" + i.foto + ".jpg");
+    return (
+      <div>
+        <img src={foto} alt={index} className="slick-image" />
+      </div>
+    )
+  })
+  return result;
+}
 
 const fotos = getImages();
 
@@ -59,7 +73,7 @@ export default function LandingPage(props) {
   return (
     <div>
       <HeaderPage change_height={400} />
-        <div
+      <div
         className={classes.pageHeader}
         style={{
           backgroundImage: "url(" + image + ")",
@@ -67,40 +81,40 @@ export default function LandingPage(props) {
           backgroundPosition: "top center"
         }}
       >
-          <div className={classes.container} >
-            <div style={{top: 50}}>
-              <GridContainer  alignItems="center"  justify="center">
-                <GridItem xs={12}>
-                  <br/>
-                  <h1 className={classes.title}>Torna-te Socio desta tuna!</h1>
-                  <h4>
-                  Antes de te tornar sócio da tuna lê os termos e condições!
-                  </h4>
-                  <br />
-                  <Button><a href={termos} download="Termos-e-condições">Download Termos e condições</a></Button>
-                </GridItem>
+        <div className={classes.container} >
+          <div style={{ top: 50 }}>
+            <GridContainer alignItems="center" justify="center">
+              <GridItem xs={12}>
                 <br />
-                <GridItem xs={12}>
-                  <br></br>
-                  <h4>
+                <h1 className={classes.title}>Torna-te Socio desta tuna!</h1>
+                <h4>
+                  Antes de te tornares sócio da tuna lê os termos e condições!
+                  </h4>
+                <br />
+                <DownloadButton color='github'href={termos} download="Termos-e-condições">Download Termos e condições</DownloadButton>
+              </GridItem>
+              <br />
+              <GridItem xs={12}>
+                <br></br>
+                <h4>
                   Após leres os termos e condições, preenche esta ficha e envia para tao.douro@gmail.com ou imprime e entrega presencialmente a um dos membros da tuna em eventos da tuna
                   </h4>
-                  <br />
-                  <Button><a href={ficha_socio} download="Ficha-de-socio">Download Ficha de sócio</a></Button>
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
+                <br />
+                <DownloadButton href={ficha_socio} download="Ficha-de-socio">Download Ficha de sócio</DownloadButton>
+              </GridItem>
+              <GridItem xs={12} sm={12} md={4}>
                 <h2 className={classes.title}>Parceiros e Benefícios</h2>
                 <Card>
-                    <Slider {...carousel_settings} className="slick-slider">
-                        {fotos}
-                    </Slider>
+                  <Slider {...carousel_settings} className="slick-slider">
+                    {fotos}
+                  </Slider>
                 </Card>
-                </GridItem>
-              </GridContainer>
-            </div>
+              </GridItem>
+            </GridContainer>
           </div>
-         </div>
-       <Footer whiteFont />
+        </div>
+      </div>
+      <Footer whiteFont />
     </div>
   );
 }
